ef355fc8a1d43bd93e6a802c53dc8b74dd05855e
Bebo0 <beboelhosary@hotmail.com>
2022-05-09 21:11:02 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/18Ot7pZfnOU_4XnJIe7msvrYUpbBND-l052zc9l_tS-Q/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyB-nEz453mlUsiJQQ1kPduux9v9_UB1Zo4';
